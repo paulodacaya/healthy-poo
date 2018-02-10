f9cffb3c1bcc0650cb0f1d7ac492afeff10bc550
paulodacaya <paulodacaya@gmail.com>
2018-02-10 22:21:05 +1100

    updated data with unique id's

--- src/data/comments.js
+++ src/data/comments.js
@@ -1,31 +1,38 @@
 const comments = [
   {
+    id: 1,
     name: "Franky",
     text: "Hey i guess i've been pooin groovie 🤗 yay!",
-    replys: [
+    children: [
       {
+        id: 2,
         name: "Brook",
         text: "I'm feeling the same way Franky!",
       },
     ],
   },
   {
+    id: 3,
     name: "Goretti",
     text: "Funny, informative, useful, and important.  People become ill and/or die because they think certain topics are embarrassing and/or taboo.",
   },
   {
+    id: 4,
     name: "Salvator",
     text: "How did I get here?!??",
   },
   {
+    id: 5,
     name: "Elvin",
     text: "Why would your poop smell musty?",
-    replys: [
+    children: [
       {
+        id: 6,
         name: "Louise",
         text: "I am experiencing that number one on the Bristol stool chart little round hard balls hard to pass this is been going on for weeks now I also have blood clots what should I do I know I need to talk to my doctor but does anyone else know besides drinking water and seeing my doctor please help",
-        replys: [
+        children: [
           {
+            id: 7,
             name: "Enes",
             text: "I am experiencing that number one on the Bristol stool chart little round hard balls hard to pass this is been going on for weeks now I also have blood clots what should I do I know I need to talk to my doctor but does anyone else know besides drinking water and seeing my doctor please help"
           },
@@ -34,10 +41,12 @@ const comments = [
     ],
   },
   {
+    id: 8,
     name: "Luffy",
     text: "Mine is always number 4 but very long sizes!",
-    replys: [
+    children: [
       {
+        id: 9,
         name: "Zoro",
         text: "That is weird Luffy!",
       },
